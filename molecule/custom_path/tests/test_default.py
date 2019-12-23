d6e08830eff5bc471cd539df8cb9d55afeef58d4
Gabriel Suarez <gabriel.suarez@solarwinds.com>
2019-12-23 17:52:10 +0000

    an scenario for testing an installation with a custom path

---
+++ molecule/custom_path/tests/test_default.py
@@ -0,0 +1,12 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_python_version(host):
+    c = host.run('/opt/installations/python3/bin/python3.8 --version')
+
+    assert '3.8.1' in c.stdout
